832a68ae7f750fd8ce5ca0377eea1092936107a9
rails --version <zaenalmuttaqien3@gmail.com>
2019-04-05 01:53:11 +0700

    modified display data that has been processed in javascript

--- application/views/home.php
+++ application/views/home.php
@@ -2,46 +2,49 @@
 	<div class="col-md-3 currency-american">
 		<img style="width: 20%;" src="<?php echo base_url(); ?>assets/image/amerika.png">
 		<span>Dollar Amerika</span>
-		<h4>USD 2.000.000</h4>
+		<h4 id="USD"></h4>
 	</div>
 	<div class="col-md-3 currency-indonesian">
 		<img style="width: 20%;" src="<?php echo base_url(); ?>assets/image/indonesia.png">
 		<span>Rupiah Indonesia</span>
-		<h4>IDR 2.000.000</h4>
+		<h4 id="IDR"></h4>
 	</div>
 	<div class="col-md-3 currency-singapore">
 		<img style="width: 20%;" src="<?php echo base_url(); ?>assets/image/Singapore.png">
 		<span>Dollar Singapore</span>
-		<h4>SGD 2.000.000</h4>
+		<h4 id="SGD"></h4>
 	</div>
 	<div class="col-md-3 currency-indonesian">
 		<img style="width: 20%;" src="<?php echo base_url(); ?>assets/image/malaysia.png">
 		<span>Ringgit Malaysia</span>
-		<h4>MYR 2.000.000</h4>
+		<h4 id="MYR"></h4>
 	</div>
 	<div class="col-md-12">
-		<span class="qq_color_title">1 Singapore Dollar Equals</span>
-		<h3>14,192.65</h3>
-		<h3>Indonesian Rupiah</h3>
-		<span class="qq_color_content">Apr 4, 4:49 AM UTC · Disclaimer</span>
+		<span class="qq_color_title">1 American Dollar Equals</span>
+		<h3 id="result-currencies-2"></h3>
+		<h3 id="result-name-currencies"></h3>
+		<span class="qq_color_content" id="date-currency"></span>
 		<div class="row qq_margin_20all">
 			<div class="col-md-2">
 				<input type="text" class="form-control" name="price" value="1" disabled>
 			</div>
 			<div class="col-md-3">
-				<select class="form-control">
-					<option value="USD">Dollar Amerika</option>
+				<select class="form-control" disabled>
+					<option value="USD">American Dollar</option>
 				</select>
 			</div>
 			<div class="col-md-1">
 				<button disabled><i class="fas fa-arrow-right"></i></button>
 			</div>
 			<div class="col-md-2">
-				<input type="text" class="form-control" name="price" value="1" disabled>
+				<input id="result-currencies" type="text" class="form-control" name="price" disabled>
 			</div>
 			<div class="col-md-3">
-				<select class="form-control">
-					<option value="USD">Dollar Amerika</option>
+				<select class="form-control" id="currency-4country">
+					<option value="USD" selected>American Dollar</option>
+					<option value="IDR">Indonesian Rupiah</option>
+					<option value="SGD">Singapore Dollar</option>
+					<option value="MYR">Ringgit Malaysia</option>
 				</select>
 			</div>
 		</div>
